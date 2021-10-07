246254172956b09a451e4a73f8c76aeae57cbeb4
houxh <houxuehua49@gmail.com>
2021-10-07 12:07:46 +0800

    add typescript declare

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+
+export interface Navigation {
+    registerComponent(screenID, generator, store?, provider?);
+    push(navigatorID, params);
+    showModal(params);
+    dismissModal(params);
+    dismissAllModals(params);
+    showLightBox(params);
+    dismissLightBox(params);
+}
+
+
+interface PushParam {
+    title?:string;
+    screen:string;
+    navigatorStyle:{navBarHidden};
+    passProps;
+}
+export interface Navigator {
+    push(param:PushParam);
+    pop();
+    setOnNavigatorEvent(cb);
+    dismissModal();
+}
