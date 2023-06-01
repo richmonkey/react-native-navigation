0e0e66bf3cbe2e1476575285f107be0f1a4e5f5b
houxh <houxuehua49@gmail.com>
2023-06-01 10:17:34 +0800

    ts declare

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,28 +1,38 @@
 
 export interface Navigation {
-    registerComponent(screenID, generator, store?, provider?);
-    push(navigatorID, params);
-    showModal(params);
-    dismissModal(params);
-    dismissAllModals(params);
-    showLightBox(params);
-    dismissLightBox(params);
+    registerComponent(screenID:string, generator:()=>any, store?, provider?):void;
+    push(navigatorID:string, params: PushParam):void;
+    showModal(params:ModalParam):void;
+    dismissModal(params):void;
+    dismissAllModals(params):void;
+    showLightBox(params):void;
+    dismissLightBox(params):void;
 }
 
+interface ModalParam {
+    title:string,
+    screen:string, 
+    navigatorStyle?:{
+        navBarHidden?:boolean,
+        tabBarHidden?:boolean,
+        statusBarHidden?:boolean,
+    }, 
+    passProps:any,
+}
 
 interface PushParam {
     title?:string;
     screen:string;
     navigatorStyle?:{
-        navBarHidden?,
-        tabBarHidden?,
-        statusBarHidden?
+        navBarHidden?:boolean,
+        tabBarHidden?:boolean,
+        statusBarHidden?:boolean,
     };
-    passProps;
+    passProps:any;
 }
 export interface Navigator {
-    push(param:PushParam);
-    pop();
-    setOnNavigatorEvent(cb);
-    dismissModal();
+    push(param:PushParam):void;
+    pop():void;
+    setOnNavigatorEvent(cb:(event:{id:string, type:string})=>void):void;
+    dismissModal():void;
 }
